ee23b6ac61774cf9ed691bf499da6bb52c85e501
Liam Sparling <las18@sfu.ca>
2022-04-06 12:33:54 -0700

    Added the new open hand model code

--- HandPoseConverted/model_processor.py
+++ HandPoseConverted/model_processor.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import sys
+import time
 
 sys.path.append('../')
 
@@ -23,13 +24,20 @@ class ModelProcessor:
 
     def predict(self, img_original):
         # preprocess image to get 'model_input'
+        start = time.time()
         model_input = self.preprocess(img_original)
+        time1 = time.time()
 
         # execute model inference
         infer_output = self.model.execute([model_input]) 
+        time2 = time.time()
 
         # postprocessing: 
         categories = self.post_process(infer_output)
+        time3 = time.time()
+        # print("Pre", time1-start)
+        # print("Model", time2-time1)
+        # print("Post", time3-time2)
 
         return categories
 
@@ -38,21 +46,24 @@ class ModelProcessor:
         preprocessing: resize image to model required size, and normalize value
         """
         # scaled_img_data = cv2.resize(img_original, (self._model_width, self._model_height))
-        # normalized_img = scaled_img_data - np.array([123, 117, 104])
+        # normalized_img = scaled_img_data / 255.0
 
-        image_expanded = np.expand_dims(img_original, axis=0)
         # Caffe model after conversion, need input to be NCHW; the orignal image is NHWC,
         # need to be transposed (use .copy() to change memory format)
         # preprocessed_img = np.asarray(normalized_img, dtype=np.float16).transpose([2,0,1]).copy()
+        # preprocessed_img = normalized_img
+        # maybe delete this line, check if it's needed
+        # preprocessed_img = np.expand_dims(preprocessed_img, axis=0)
+
+        preprocessed_img = cv2.dnn.blobFromImage(img_original, 1.0/255, (self._model_width, self._model_height), (0, 0, 0), swapRB=False, crop=False)
         
-        return image_expanded
+        return preprocessed_img
 
     def post_process(self, infer_output):
-        print("we reached post-processing")
-        print(self._model_width)
+        print(infer_output[0].shape)
         # data = infer_output[0]
         # vals = data.flatten()
         # top_k = vals.argsort()[-1:-6:-1]
-        return infer_output, 1
+        return infer_output[0]
 
 
